7a0c4713eeb2930d870bb1e765d8af548bbef0ae
WonJung Kim <95566897+kimwonj77@users.noreply.github.com>
2023-04-10 13:39:49 +0900

    with mono and non mono

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+args="--complete --also-windows --no-progressbars --careful  --fontawesome --fontawesomeextension --fontlogos --octicons --codicons --powersymbols --pomicons --powerline --powerlineextra --material --weather"
+
 mkdir font-out
 
 run_root=0
@@ -8,16 +10,14 @@ for f in font-in/*; do
     if [ -d "$f" ]; then
         d=$(basename $f)
         mkdir $(pwd)/font-out/$d
-        docker run --rm -v $(pwd)/font-in/$d:/in -v $(pwd)/font-out/$d:/out nerdfonts/patcher \
-            --mono --complete --careful --also-windows --no-progressbars \
-            --fontawesome --fontawesomeextension --fontlogos --octicons --codicons --powersymbols --pomicons --powerline --powerlineextra --material --weather
+        docker run --rm -v $(pwd)/font-in/$d:/in -v $(pwd)/font-out/$d:/out nerdfonts/patcher $args
+        docker run --rm -v $(pwd)/font-in/$d:/in -v $(pwd)/font-out/$d:/out nerdfonts/patcher --mono $args
     else
         run_root=1
     fi
 done
 
 if [ "$run_root" == 1 ]; then
-    docker run --rm -v $(pwd)/font-in:/in -v $(pwd)/font-out:/out nerdfonts/patcher \
-        --mono --complete --careful --also-windows --no-progressbars \
-        --fontawesome --fontawesomeextension --fontlogos --octicons --codicons --powersymbols --pomicons --powerline --powerlineextra --material --weather
+    docker run --rm -v $(pwd)/font-in:/in -v $(pwd)/font-out:/out nerdfonts/patcher $args
+    docker run --rm -v $(pwd)/font-in:/in -v $(pwd)/font-out:/out nerdfonts/patcher --mono $args
 fi
