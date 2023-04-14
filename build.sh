d481cb4fb46aa55aabaaf477381358f0f85b94c6
WonJung Kim <95566897+kimwonj77@users.noreply.github.com>
2023-04-14 12:40:19 +0900

    remove mono as it's buggy and also d2 is mono already.

--- build.sh
+++ build.sh
@@ -11,7 +11,6 @@ for f in font-in/*; do
         d=$(basename $f)
         mkdir $(pwd)/font-out/$d
         docker run --rm -v $(pwd)/font-in/$d:/in -v $(pwd)/font-out/$d:/out nerdfonts/patcher $args
-        docker run --rm -v $(pwd)/font-in/$d:/in -v $(pwd)/font-out/$d:/out nerdfonts/patcher --mono $args
     else
         run_root=1
     fi
@@ -19,5 +18,4 @@ done
 
 if [ "$run_root" == 1 ]; then
     docker run --rm -v $(pwd)/font-in:/in -v $(pwd)/font-out:/out nerdfonts/patcher $args
-    docker run --rm -v $(pwd)/font-in:/in -v $(pwd)/font-out:/out nerdfonts/patcher --mono $args
 fi
